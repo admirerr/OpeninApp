57088af551767e9ff50469be79202bf913408a66
Shubham <shubhamkashyap0608@gmail.com>
2024-02-02 15:49:36 +0530

    Fixed issues till Soft Deletion of Tasks and Subtasks

--- index.js
+++ index.js
@@ -15,28 +15,34 @@ const secretKey = 'YOUR_SECRET_KEY';
 
 mongoose.connect('YOUR_MONGO_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
+
 // Task schema Structure
 const taskSchema = new mongoose.Schema({
   task_id: String,
   title: String,
   description: String,
+  created_at: Date,
+  updated_at: Date,
+  deleted_at: Date,
   due_date: Date,
   status: String,
+  priority: Number,
 });
 
 const Task = mongoose.model('Task', taskSchema);
 
-
+// Subtask schema Structure
 const subTaskSchema = new mongoose.Schema({
-    id: String,
-    task_id: String,
-    title: String,
-    description: String,
-    due_date: Date,
-    status: String,
-  });
-  
+  subtask_id: String,
+  task_id: String,
+  status: Number,
+  created_at: Date,
+  updated_at: Date,
+  deleted_at: Date,
+});
+
 const SubTask = mongoose.model('SubTask', subTaskSchema);
+
   
 
 // Middleware for JWT authentication
@@ -74,15 +80,21 @@ app.post('/login', (req, res) => {
 
 // Route to create a new task
 app.post('/tasks', authenticateToken, async (req, res) => {
-    const { title, description, due_date, status } = req.body;
+    const { title, description, due_date} = req.body;
+    // Get the current timestamp
+    const currentTimestamp = new Date();
   
     try {
       const newTask = new Task({
-        task_id: uuid.v4(), // Generating a unique task_id
+        task_id: uuid.v4(),
         title,
         description,
         due_date,
-        status,
+        created_at: currentTimestamp,
+        updated_at: currentTimestamp,
+        deleted_at: null,
+        status: "TODO",
+        priority: null,
       });
   
       await newTask.save();
@@ -96,15 +108,17 @@ app.post('/tasks', authenticateToken, async (req, res) => {
 // Route to create a new subtask with a specific task_id
 app.post('/subtasks/:task_id', authenticateToken, async (req, res) => {
     const { task_id } = req.params;
-    const { title, description, due_date, status } = req.body;
+    // Get the current timestamp
+    const currentTimestamp = new Date();
   
     try {
       const newSubTask = new SubTask({
+        subtask_id: uuid.v4(),
         task_id,
-        title,
-        description,
-        due_date,
-        status,
+        status: 0,
+        created_at: currentTimestamp,
+        updated_at: currentTimestamp,
+        deleted_at: null,
       });
   
       await newSubTask.save();
@@ -180,13 +194,13 @@ app.put('/tasks/:task_id', authenticateToken, async (req, res) => {
 
 
   // Route to update a subtask
-app.put('/subtasks/:_id', authenticateToken, async (req, res) => {
-    const { _id } = req.params;
+app.put('/subtasks/:subtask_id', authenticateToken, async (req, res) => {
+    const { subtask_id } = req.params;
     const { status } = req.body;
   
     try {
       const updatedSubtask = await SubTask.findOneAndUpdate(
-        { _id },
+        { subtask_id },
         { $set: { status } },
         { new: true }
       );
@@ -206,11 +220,13 @@ app.put('/subtasks/:_id', authenticateToken, async (req, res) => {
 app.delete('/tasks/:task_id', authenticateToken, async (req, res) => {
     const { task_id } = req.params;
   
+    // Get the current timestamp
+    const currentTimestamp = new Date();
     try {
       // Soft delete by updating the status to "DELETED"
       const updatedTask = await Task.findOneAndUpdate(
         { task_id },
-        { $set: { status: "DELETED" } },
+        { $set: { deleted_at: currentTimestamp } },
         { new: true }
       );
   
@@ -226,14 +242,14 @@ app.delete('/tasks/:task_id', authenticateToken, async (req, res) => {
 
 
 // Route to soft delete a subtask
-app.delete('/subtasks/:_id', authenticateToken, async (req, res) => {
-    const { _id } = req.params;
+app.delete('/subtasks/:subtask_id', authenticateToken, async (req, res) => {
+    const { subtask_id } = req.params;
   
     try {
       // Soft delete by updating the status to "DELETED"
       const updatedSubtask = await SubTask.findOneAndUpdate(
-        { _id },
-        { $set: { status: "DELETED" } },
+        { subtask_id },
+        { $set: { status: 1 } },
         { new: true }
       );
   
